cb93b46aebb5c09f5fb06b765e1b92878ec7b67a
arda-saratas <darklight96.38@gmail.com>
2024-02-29 22:46:31 +0300

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-[100vw] h-[100vw] absolute left-0 top-0">
+
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        Arda Saratas
+        <div className="absolue left-0 bottom-0 h-[300px] w-[400px] bg-slate-800"></div>
+      </main>
+    </div>
+  );
+}
